4b2566704533fd139de882ddaa7c75d2fc5aa769
Carlos Delvalle <carlosdelvalle89@gmail.com>
2014-09-07 16:44:14 -0300

    Agrego pruebaReiniciar

--- taller/main.cpp
+++ taller/main.cpp
@@ -3,12 +3,14 @@
 #include "prueba_VistaEscenario.h"
 #include "pruebapoligono.h"
 #include "pruebaParser.h"
+#include "pruebaReiniciar.h"
 #undef main
 //#include <vld.h>
 int main(int argc, char ** argv){
-	prueba_vistaEscenario();
+	//prueba_vistaEscenario();
 	//caca();
 	//caca2();
 	//pruebaParser();
+	pruebaReiniciar();
 	return 0;
 }
