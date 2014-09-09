140ece3d4b85b3862e51a5732a296cd851cea641
unknown <carlosfurnari@gmail.com>
2014-09-09 01:40:51 -0300

    Se agrega una beta de la animacion del personaje, falta saltar y otras mejoras para que se vea el movimiento correctamente

--- taller/vista/VistaJugador.cpp
+++ taller/vista/VistaJugador.cpp
@@ -1,6 +1,5 @@
 #include "VistaJugador.h"
 
-
 VistaJugador::VistaJugador(SDL_Renderer* renderer,Jugador* jugador,DatosPantalla* datos)
 {
 	this->m_jugador= jugador;
@@ -11,9 +10,233 @@ VistaJugador::VistaJugador(SDL_Renderer* renderer,Jugador* jugador,DatosPantalla
 }
 
 void VistaJugador::render(){
-	this->transformarSint16(this->m_jugador->getVertices());
-	filledPolygonRGBA(m_renderer, m_vx , m_vy ,4,0,100,255,255);
+	CoordenadasR2 size = m_jugador->getSize(); //ancho,alto
+	CoordenadasR2 pos = m_jugador->getPosicion();//x,y
+	this->agregarSprite("imagenes/sprite.png");
+	int x,y,w,h;
+	x = ( pos.x - ( size.x / 2 ) ) * this->m_datos->getXratio();
+	y = this->m_datos->getAltoPixel() - ( (pos.y + ( size.y / 2 ) ) * this->m_datos->getYratio());
+	w = size.x * this->m_datos->getXratio();
+	h = size.y * this->m_datos->getYratio();
+	//SDL_Rect* renderQuad = new SDL_Rect;
+	//renderQuad->x = x;
+	//renderQuad->y = y;
+	//renderQuad->h = h;
+	//renderQuad->w = w;
+	this->cargarSprites(m_jugador->getDireccion(), x, y, w, h); // se podrian cargar todas antes, teniendo un SDL_rect para cada posicion
+	if ((this->m_jugador->getDireccion() == IZQUIERDA) || (this->m_jugador->getDireccion() == ARRIBA_IZQUIERDA) ){
+		m_dirAnterior = IZQUIERDA;
+	}else if ((this->m_jugador->getDireccion() == DERECHA) || (this->m_jugador->getDireccion() == ARRIBA_IZQUIERDA) ){
+		m_dirAnterior = DERECHA;
+	}
+	//delete renderQuad;
+
 }
+
+void VistaJugador::cargarSprites(int dire, int x, int y, int w, int h){
+	int i,j;
+	SDL_Rect renderQuad = { x, y, w, h };
+	switch(dire) 
+	{
+	case IZQUIERDA : 
+		m_spriteClips[ 1 ].x =  32;
+		m_spriteClips[ 1 ].y =  58;
+		m_spriteClips[ 1 ].w =  21;
+		m_spriteClips[ 1 ].h =  24;
+		
+		m_spriteClips[ 2 ].x =  56;
+		m_spriteClips[ 2 ].y =  58;
+		m_spriteClips[ 2 ].w =  21;
+		m_spriteClips[ 2 ].h =  24;
+
+		m_spriteClips[ 3 ].x =  87;
+		m_spriteClips[ 3 ].y =  58;
+		m_spriteClips[ 3 ].w =  21;
+		m_spriteClips[ 3 ].h =  24;
+		
+		for(i=0;i<3;i++){
+			//for (j=0;j<4;j++){
+				//SDL_SetRenderDrawColor( this->m_renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+				//SDL_RenderClear( this->m_renderer );
+				SDL_RenderCopy( this->m_renderer, this->m_sprite, &m_spriteClips[1+i], &renderQuad );
+				SDL_RenderPresent(this->m_renderer);
+			//}
+		}
+		break;
+	case DERECHA :
+		m_spriteClips[ 1 ].x =  56; 
+		m_spriteClips[ 1 ].y =  82;
+		m_spriteClips[ 1 ].w =  21;
+		m_spriteClips[ 1 ].h =  24;
+		
+		m_spriteClips[ 2 ].x =  32; 
+		m_spriteClips[ 2 ].y =  82;
+		m_spriteClips[ 2 ].w =  21;
+		m_spriteClips[ 2 ].h =  24;
+
+		m_spriteClips[ 3 ].x =   0; 
+		m_spriteClips[ 3 ].y =  82;
+		m_spriteClips[ 3 ].w =  21;
+		m_spriteClips[ 3 ].h =  24;
+		
+		for(i=0;i<3;i++){
+			//for (j=0;j<4;j++){
+				//SDL_SetRenderDrawColor( this->m_renderer, 0xFF, 0xFF, 0xFF, 0xFF );
+				//SDL_RenderClear( this->m_renderer );
+				SDL_RenderCopy( this->m_renderer, this->m_sprite, &m_spriteClips[1+i], &renderQuad );
+				SDL_RenderPresent(this->m_renderer);
+			//}
+		}
+		break;
+	case ARRIBA_IZQUIERDA:
+		m_spriteClips[ 0 ].x =   0;
+		m_spriteClips[ 0 ].y =   0;
+		m_spriteClips[ 0 ].w =  23;
+		m_spriteClips[ 0 ].h =  29;
+
+		m_spriteClips[ 1 ].x =  32;
+		m_spriteClips[ 1 ].y =   0;
+		m_spriteClips[ 1 ].w =  23;
+		m_spriteClips[ 1 ].h =  29;
+		
+		m_spriteClips[ 2 ].x =  64;
+		m_spriteClips[ 2 ].y =   0;
+		m_spriteClips[ 2 ].w =  23;
+		m_spriteClips[ 2 ].h =  29;
+
+		m_spriteClips[ 3 ].x =  96;
+		m_spriteClips[ 3 ].y =   0;
+		m_spriteClips[ 3 ].w =  23;
+		m_spriteClips[ 3 ].h =  29;
+		
+		m_spriteClips[ 4 ].x = 128;
+		m_spriteClips[ 4 ].y =   0;
+		m_spriteClips[ 4 ].w =  23;
+		m_spriteClips[ 4 ].h =  29;
+		
+		for(i=0;i<5;i++){
+			for (j=0;j<4;j++){
+				SDL_RenderCopy( this->m_renderer, this->m_sprite, &m_spriteClips[i], &renderQuad );
+				SDL_RenderPresent(this->m_renderer);
+			}
+		}
+		break;
+
+	case ARRIBA_DERECHA:
+		m_spriteClips[ 0 ].x = 128;
+		m_spriteClips[ 0 ].y =   0;
+		m_spriteClips[ 0 ].w =  23;
+		m_spriteClips[ 0 ].h =  29;
+
+		m_spriteClips[ 1 ].x =  96;
+		m_spriteClips[ 1 ].y =   0;
+		m_spriteClips[ 1 ].w =  23;
+		m_spriteClips[ 1 ].h =  29;
+		
+		m_spriteClips[ 2 ].x =  64;
+		m_spriteClips[ 2 ].y =   0;
+		m_spriteClips[ 2 ].w =  23;
+		m_spriteClips[ 2 ].h =  29;
+
+		m_spriteClips[ 3 ].x =  32;
+		m_spriteClips[ 3 ].y =   0;
+		m_spriteClips[ 3 ].w =  23;
+		m_spriteClips[ 3 ].h =  29;
+		
+		m_spriteClips[ 4 ].x =   0;
+		m_spriteClips[ 4 ].y =   0;
+		m_spriteClips[ 4 ].w =  23;
+		m_spriteClips[ 4 ].h =  29;
+		
+		for(i=0;i<5;i++){
+			for (j=0;j<4;j++){
+				SDL_RenderCopy( this->m_renderer, this->m_sprite, &m_spriteClips[i], &renderQuad );
+				SDL_RenderPresent(this->m_renderer);
+			}
+		}
+		break;
+		
+	case ARRIBA:
+		if (m_dirAnterior = IZQUIERDA) {
+		m_spriteClips[ 0 ].x =   0;
+		m_spriteClips[ 0 ].y =   0;
+		m_spriteClips[ 0 ].w =  23;
+		m_spriteClips[ 0 ].h =  29;
+
+		m_spriteClips[ 1 ].x =  32;
+		m_spriteClips[ 1 ].y =   0;
+		m_spriteClips[ 1 ].w =  23;
+		m_spriteClips[ 1 ].h =  29;
+		
+		m_spriteClips[ 2 ].x =  64;
+		m_spriteClips[ 2 ].y =   0;
+		m_spriteClips[ 2 ].w =  23;
+		m_spriteClips[ 2 ].h =  29;
+
+		m_spriteClips[ 3 ].x =  96;
+		m_spriteClips[ 3 ].y =   0;
+		m_spriteClips[ 3 ].w =  23;
+		m_spriteClips[ 3 ].h =  29;
+		
+		m_spriteClips[ 4 ].x = 128;
+		m_spriteClips[ 4 ].y =   0;
+		m_spriteClips[ 4 ].w =  23;
+		m_spriteClips[ 4 ].h =  29;
+		}
+		else{
+		m_spriteClips[ 0 ].x = 128;
+		m_spriteClips[ 0 ].y =   0;
+		m_spriteClips[ 0 ].w =  23;
+		m_spriteClips[ 0 ].h =  29;
+
+		m_spriteClips[ 1 ].x =  96;
+		m_spriteClips[ 1 ].y =   0;
+		m_spriteClips[ 1 ].w =  23;
+		m_spriteClips[ 1 ].h =  29;
+		
+		m_spriteClips[ 2 ].x =  64;
+		m_spriteClips[ 2 ].y =   0;
+		m_spriteClips[ 2 ].w =  23;
+		m_spriteClips[ 2 ].h =  29;
+
+		m_spriteClips[ 3 ].x =  32;
+		m_spriteClips[ 3 ].y =   0;
+		m_spriteClips[ 3 ].w =  23;
+		m_spriteClips[ 3 ].h =  29;
+		
+		m_spriteClips[ 4 ].x =   0;
+		m_spriteClips[ 4 ].y =   0;
+		m_spriteClips[ 4 ].w =  23;
+		m_spriteClips[ 4 ].h =  29;
+		}
+
+		for(i=0;i<5;i++){
+			for (j=0;j<4;j++){
+				SDL_RenderCopy( this->m_renderer, this->m_sprite, &m_spriteClips[i], &renderQuad );
+				SDL_RenderPresent(this->m_renderer);
+			}
+		}
+		break;
+	case ESTATICO : 
+		if (m_dirAnterior == IZQUIERDA) {
+			m_spriteClips[ 0 ].x =   0;
+			m_spriteClips[ 0 ].y =  58;
+			m_spriteClips[ 0 ].w =  21;
+			m_spriteClips[ 0 ].h =  24;
+		} else {
+			m_spriteClips[ 0 ].x =  88; 
+			m_spriteClips[ 0 ].y =  82;
+			m_spriteClips[ 0 ].w =  21;
+			m_spriteClips[ 0 ].h =  24;
+		}
+		SDL_RenderCopy( this->m_renderer, this->m_sprite, &m_spriteClips[0], &renderQuad );
+		SDL_RenderPresent(this->m_renderer);
+		break;
+	}
+
+}
+
+
 VistaJugador::~VistaJugador(void)
 {
 	delete[] m_vx;
@@ -27,4 +250,40 @@ void VistaJugador::transformarSint16(std::vector<CoordenadasR2> vertices){
 			m_vy[i]= this->m_datos->getAltoPixel() -(vertices[i].y *  this->m_datos->getYratio());
 		} 
 		
+}
+
+void VistaJugador::agregarSprite(std::string path){
+	
+	//if(m_sprite != NULL){
+	//	SDL_DestroyTexture( m_sprite );
+	//}
+	//Levanto los sprites
+	SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+	if( loadedSurface == NULL )
+	{
+		printf( "No se pudo levantar la imagen de sprite %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+	}
+	else
+	{
+		// Asigno el color clave de la imagen
+		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 186, 254, 202 ) );
+
+		//Creo textura a partir de la surface
+        m_sprite = SDL_CreateTextureFromSurface( m_renderer, loadedSurface );
+		if( m_sprite == NULL )
+		{
+			printf( "No se pudo crear la textura desde %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+		}
+		else
+		{
+			//Me guardo la dimension de la imagen
+			ms_ancho = loadedSurface->w;
+			ms_alto = loadedSurface->h;
+		}
+
+		//Elimino la surface temporal
+		SDL_FreeSurface( loadedSurface );
+	}
+
+
 }
--- taller/vista/VistaJugador.h
+++ taller/vista/VistaJugador.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "..\model\Jugador.h"
+#include "../model/constantes.h"
 #include "SDL2_gfx\SDL2_gfxPrimitives.h"
 #include "VistaFigura.h"
 #include "../model/CoordenadasR2.h"
 #include <SDL.h>
+#include <SDL2\SDL_image.h>
 #include "DatosPantalla.h"
 #include <vector>
 class VistaJugador: public VistaFigura
@@ -11,11 +13,17 @@ class VistaJugador: public VistaFigura
 public: 
 	VistaJugador(SDL_Renderer* renderer ,Jugador* jugador,DatosPantalla* datos);
 	void render();
+	void agregarSprite(std::string path);
 	~VistaJugador(void);
 private:
 	void transformarSint16(std::vector<CoordenadasR2> vertices);
+	void cargarSprites(int dire, int x, int y, int w, int h);
 	Jugador* m_jugador;
+	SDL_Rect m_spriteClips[ 5 ];
+	SDL_Texture* m_sprite;
 	Sint16* m_vx;
 	Sint16* m_vy;
+	int ms_ancho;
+	int ms_alto;
+	int m_dirAnterior;
 };
-
