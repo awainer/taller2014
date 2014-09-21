e19adf3e4e577cf2435c8f615416a963c50e6f26
awainer <awainer@gmail.com>
2014-09-21 17:18:07 -0300

    agrego metodo para parsear circulo

--- taller/parser/Parser.h
+++ taller/parser/Parser.h
@@ -27,6 +27,7 @@ private:
 	bool parsearBoolean(Json::Value elem, bool defaultVal, string nombreElem);
 	poli parsearPoligono(Json::Value elem);
 	rect parsearRectangulo(Json::Value elem);
+	circ parsearCirculo(Json::Value elem);
 	colorRGB parsearColor(Json::Value elem, colorRGB defaultVal, string nombreElem);
 	unsigned int parsearAngulo(Json::Value elem, unsigned int defaultVal, string nombreElem);
 
