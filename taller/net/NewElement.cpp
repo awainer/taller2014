9041388658a9ed4fc7672930afc0cd6bf23f8fb0
awainer <awainer@gmail.com>
2014-09-28 15:41:29 -0300

    Constructores para newElement desde el modelo

--- taller/net/NewElement.cpp
+++ taller/net/NewElement.cpp
@@ -1,9 +1,38 @@
 #include "NewElement.h"
 
 
+
+NewElement::NewElement(Pelota * pelota){
+	this->id = pelota->id;
+	this->color = pelota->color;
+	this->type = CIRCULO;
+	this->radio = pelota->getRadio();
+	this->vertices[0] = pelota->getCentro();
+	this->vertices[1] = pelota->getPuntoReferencia();
+}
+
+NewElement::NewElement(Jugador * jugador){
+	this->id = jugador->id;
+	this->type = JUGADOR;
+	this->alto = jugador->getSize().y;
+	this->ancho = jugador->getSize().x;
+	this->direccion = jugador->getDireccion();
+}
+
+NewElement::NewElement(Poligono * poligono){
+	this->id = poligono->id;
+	this->color = poligono->color;
+	this->type = POLIGONO;
+	this->lados = poligono->getVertexCount();
+	std::vector<CoordenadasR2> aux_vec = poligono->getVertices();
+	for (unsigned int i = 0; i < this->lados; i++)
+		this->vertices[i] = aux_vec[i];
+}
+
 NewElement::NewElement(void)
 {
 }
+
 
 
 NewElement::~NewElement(void)
--- taller/net/NewElement.h
+++ taller/net/NewElement.h
@@ -3,12 +3,18 @@
 #include "../constantes.h"
 #include "../model/CoordenadasR2.h"
 #include "../model/Color.h"
+#include "../model/Pelota.h"
+#include "../model/Jugador.h"
+#include "../model/Poligono.h"
 
 class NewElement :
 	public Packet
 {
 public:
 	NewElement(void);
+	NewElement(Pelota * pelota);
+	NewElement(Jugador * jugador);
+	NewElement(Poligono * poligono);
 	~NewElement(void);
 	unsigned int id, lados;
 	TipoFigura tipo;
