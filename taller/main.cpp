6b012d0fae9af6c769096a53ab980325c8530e45
awainer <awainer@gmail.com>
2014-09-28 21:21:54 -0300

    prueba en threads server/client

--- taller/main.cpp
+++ taller/main.cpp
@@ -4,21 +4,39 @@
 #include "test\pruebapoligono.h"
 #include "test\testPositionHandler.h"
 #include "Server.h"
+#include "Client.h"
 #undef main
+#include <SDL_thread.h>
+
+int run_server(void * data){
+	Server server;
+	server.init("");
+	return 0;
+}
+
+int run_client(void * data){
+	Client client;
+	client.init();
+	client.connectar();
+
+	return 0;
+}
 
 int main(int argc, char ** argv){
-	string path;
-	Server server;
-	
+		
+	SDL_Thread *server = NULL;
+	SDL_Thread *client = NULL;
 
-	log("Logger inicializado",DEBUG);
+	server = SDL_CreateThread(run_server, "server",NULL );
+	server = SDL_CreateThread(run_client, "client",NULL );
+/*	log("Logger inicializado",DEBUG);
 
 	if (argc != 2){
 		log("Cantidad de argumentos incorrecta!",ERROR);
 		path = "";
 	}else
-		 path = string(argv[1]);
-	server.init(path);
+		 path = string(argv[1]);*/
+	
 	//pruebaPositionHandler();
 	//caca();
 	//gameLoop(path);
